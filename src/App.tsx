401bb9d32d3558aead8fdf7c3d4ad4e63edd62da
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-13 15:08:04 +0530

    Code QL check

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState({ call: '' });
-  state.call = 'as';
+  const [state, setState] = useState({ call: 1 });
+  const tick = () => {
+    state.call += 1;
+  }
+  useEffect(() => {
+    setInterval(() => tick(), 1000);
+  }, []);
   return (
     <div className="App">
       <header className="App-header" data-testid="header-element">
-        Empty
+        {state.call}
       </header>
     </div>
   );
