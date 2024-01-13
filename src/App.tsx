30074d2ad78ee72509e3323af2dc416f12f2cb82
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-13 15:05:01 +0530

    Code QL check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({ call: '' });
+  state.call = 'as';
   return (
     <div className="App">
       <header className="App-header" data-testid="header-element">
