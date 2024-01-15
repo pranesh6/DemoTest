df75757c194dd91186ca0da6b7339a179aa05b2c
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-15 15:17:33 +0530

    here

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState({ call: 1 });
-  const tick = () => {
-    state.call += 1;
-  }
-
-  useEffect(() => {
-    setInterval(() => tick(), 1000);
-  }, []);
   return (
     <div className="App">
       <header className="App-header" data-testid="header-element">
-        {state.call}
+        Empty
       </header>
-      <div alt="
-<x" title="/>
-<img src=url404 onerror=alert(1)>"/>
-      <div
-        dangerouslySetInnerHTML={{__html: ''}}
-      />
     </div>
   );
 }
