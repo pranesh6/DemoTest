d5c8844fc74ce769dd9636e41c84bb825f32c28b
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-15 15:21:51 +0530

    here

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header" data-testid="header-element">
-        Empty
+        Expected Output
       </header>
     </div>
   );
