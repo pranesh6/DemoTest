4dad544511889ad4178b15695d7a6abaeeab6b5e
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-15 22:43:01 +0530

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './App.css'
+import { Typography } from '@mui/material'
+import { styled } from '@mui/material/styles';
+
+const Div = styled('div')(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(1)
+}))
+
 const headerText = [,1,,2]
 
 function App (): JSX.Element {
   const headerText = 'Expected Output'
   return (
-    <div className="App">
-      <header className="App-header" data-testid="header-element">
+    <Div>
+      <Typography variant='h1' data-testid="header-element">
         {headerText}
-      </header>
-    </div>
+      </Typography>
+    </Div>
   )
 }
 
