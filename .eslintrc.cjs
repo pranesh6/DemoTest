2789b9f4c1697558fad7d85c4f285ccbc8510641
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-16 09:29:31 +0530

    Eslint updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,36 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['standard-with-typescript', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
   env: {
+    browser: true,
+    es2023: true,
     node: true
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   }
-};
+}
