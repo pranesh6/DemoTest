fd5574ad752432956284217a269f4be2a544ea10
pranesh06011999 <99378737+pranesh06011999@users.noreply.github.com>
2024-01-17 21:30:32 +0530

    Automate test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('Show expected output', () => {
-  render(<App />);
-  const element = screen.getByTestId('header-element');
-  expect(element).toHaveTextContent('Expected Output');
-});
+  render(<App />)
+  const element = screen.getByTestId('header-element')
+  expect(element).toHaveTextContent('Expected Output')
+})
